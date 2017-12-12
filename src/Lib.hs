20bb33bb6f0c3970030ec09256b65c2f97037ef5
abhimanyuhazarika <32271162+abhimanyuhazarika@users.noreply.github.com>
2017-12-12 23:43:16 +0000

    Update Lib.hs

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,3 @@
-
 {-# LANGUAGE BangPatterns    #-}
 {-# LANGUAGE TemplateHaskell #-}
 --{-# CPP #-}
@@ -29,107 +28,45 @@ import           PrimeFactors
 import           System.Environment                                 (getArgs)
 import           System.Exit
 
--- this is the work we get workers to do. It could be anything we want. To keep things simple, we'll calculate the
--- number of prime factors for the integer passed.
-doWork :: Integer -> Integer
-doWork = numPrimeFactors
-
 -- | worker function.
 -- This is the function that is called to launch a worker. It loops forever, asking for work, reading its message queue
 -- and sending the result of runnning numPrimeFactors on the message content (an integer).
-
-worker:: (ProcessId, NodeId, String) -> Process ()
+worker :: (ProcessId, NodeId, String) -> Process ()
 worker (master, workerId, url) = do
-  liftIO ( putStrLn $ "Starting worker : " ++ (show workerId) ++ " with parameter: " ++ url)
+  liftIO ( putStrLn $ "Worker : " ++ (show workerId) ++ " started with parameter: " ++ url) 
   let repoName = last $ splitOn "/" url
-  gitRepoExists <- liftIO $ doesDirectoryExist ("/tmp/" ++ repoName)
+  gitRepoExists <- liftIO $ doesDirectoryExist ("/test/" ++ repoName)
   if not gitRepoExists then do
-    liftIO $ callProcess "/usr/bin/git" ["clone", url, "/tmp/" ++ repoName]
+    liftIO $ callProcess "/usr/bin/git" ["clone", url, "/test/" ++ repoName]
+  else do
+    liftIO $ putStrLn "Repository exists!"
   let conf = (Config 6 [] [] [] Colored)
-  let source = allFiles ("/tmp/" ++ repoName)
+  let source = allFiles ("/test/" ++ repoName)
               >-> P.mapM (liftIO . analyze conf)
               >-> P.map (filterResults conf)
               >-> P.filter filterNulls
-  liftIO $ putStrLn $ "Launching analyse for " ++ url
+  liftIO $ putStrLn $ "Analyse Started for" ++ url
   (output, _) <- liftIO $ capture $ runSafeT $ runEffect $ exportStream conf source
-  liftIO ( putStrLn $ "End of worker : " ++ (show workerId) ++ " with parameter: " ++ url)
+  liftIO ( putStrLn $ "Worker : " ++ (show workerId) ++ " finished work with parameter: " ++ url)
   send master $ (workerId, url, output)
 
-#remotable ['worker] -- this makes the worker function executable on a remote node
+remotable ['worker]
 
-manager :: Integer    -- The number range we wish to generate work for (there will be n work packages)
-        -> [NodeId]   -- The set of cloud haskell nodes we will initalise as workers
-        -> Process Integer
-manager n workers = do
-  us <- getSelfPid
+myRemoteTable :: RemoteTable
+myRemoteTable = Main.__remoteTable initRemoteTable
 
-  -- first, we create a thread that generates the work tasks in response to workers
-  -- requesting work.
-  workQueue <- spawnLocal $ do
-    -- Return the next bit of work to be done
-    forM_ [1 .. n] $ \m -> do
-      pid <- expect   -- await a message from a free worker asking for work
-      send pid m     -- send them work
-
-    -- Once all the work is done tell the workers to terminate. We do this by sending every worker who sends a message
-    -- to us a null content: () . We do this only after we have distributed all the work in the forM_ loop above. Note
-    -- the indentiation - this is part of the workQueue do block.
-    forever $ do
-      pid <- expect
-      send pid ()
-
-  -- Next, start worker processes on the given cloud haskell nodes. These will start
-  -- asking for work from the workQueue thread immediately.
-  forM_ workers $ \ nid -> spawn nid ($(mkClosure 'worker) (us, workQueue))
-  liftIO $ putStrLn $ "[Manager] Workers spawned"
-  -- wait for all the results from the workers and return the sum total. Look at the implementation, whcih is not simply
-  -- summing integer values, but instead is expecting results from workers.
-  sumIntegers (fromIntegral n)
-
--- note how this function works: initialised with n, the number range we started the program with, it calls itself
--- recursively, decrementing the integer passed until it finally returns the accumulated value in go:acc. Thus, it will
--- be called n times, consuming n messages from the message queue, corresponding to the n messages sent by workers to
--- the manager message queue.
-sumIntegers :: Int -> Process Integer
-sumIntegers = go 0
-  where
-    go :: Integer -> Int -> Process Integer
-    go !acc 0 = return acc
-    go !acc n = do
-      m <- expect
-      go (acc + m) (n - 1)
-
-rtable :: RemoteTable
-rtable = Lib.__remoteTable initRemoteTable
-
--- | This is the entrypoint for the program. We deal with program arguments and launch up the cloud haskell code from
--- here.
-someFunc :: IO ()
-someFunc = do
-
-
+main :: IO ()
+main = do
   args <- getArgs
-
   case args of
-    ["manager", host, port, n] -> do
-      putStrLn "Starting Node as Manager"
-      backend <- initializeBackend host port rtable
-      startMaster backend $ \workers -> do
-        result <- manager (read n) workers
-        liftIO $ print result
-    ["worker", host, port] -> do
-      putStrLn "Starting Node as Worker"
-      backend <- initializeBackend host port rtable
+    ["master", host, port] -> do
+      start <- getCurrentTime
+      backend <- initializeBackend host port myRemoteTable
+      startMaster backend (master backend)
+      end <- getCurrentTime
+      let timetaken = diffUTCTime end start
+      appendFile "record.txt" (show timetaken ++ ",")
+    ["slave", host, port] -> do
+      backend <- initializeBackend host port myRemoteTable
       startSlave backend
-    _ -> putStrLn "Bad parameters"
 
-
-  -- create a cloudhaskell node, which must be initialised with a network transport
-  -- Right transport <- createTransport "127.0.0.1" "10501" defaultTCPParameters
-  -- node <- newLocalNode transport initRemoteTable
-
-  -- runProcess node $ do
-  --   us <- getSelfNode
-  --   _ <- spawnLocal $ sampleTask (1 :: Int, "using spawnLocal")
-  --   pid <- spawn us $ $(mkClosure 'sampleTask) (1 :: Int, "using spawn")
-  --   liftIO $ threadDelay 2000000
